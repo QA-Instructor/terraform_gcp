9ec84b4c6c66e41babf26c3b2b53cb2793fb27ce
unknown <qainstructorsky@gmail.com>
2025-06-16 17:19:07 +0100

    EDIT: added unique subnet name

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ resource "google_compute_network" "lbg-network" {
 }
 
 resource "google_compute_subnetwork" "subnet1" {
-  name = "subnet1"
+  name = var.subnetwork_name
   ip_cidr_range = "10.0.1.0/24"
   region = var.region
   network = google_compute_network.lbg-network.id
--- variables.tf
+++ variables.tf
@@ -28,6 +28,10 @@ variable "network_name" {
   type = string
 }
 
+variable "subnetwork_name" {
+  type = string
+}
+
 variable "vm_name" {
   type = string
 }
